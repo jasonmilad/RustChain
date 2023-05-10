9f0c9a2f59304ff9aa29e5f62d5b71c331741893
Jason Milad <jasonmilad29@gmail.com>
2023-05-10 13:41:15 -0400

    added struct variables

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,22 @@
 struct Block {
+    block_size: u32,
+    block_header: BlockHeader,
+    transaction_count: u32,
+    transactions: Vec<Transaction>,
+}
+
+impl Block {
+}
+
+struct BlockHeader {
+    version: u32,
+    previous_block_hash: [u8; 32],
+    merkle_root_hash: [u8; 32],
+    timestamp: u32,
+    difficulty_target: u32,
+    nonce: u32,
+}
+
+impl BlockHeader {
     
 }
