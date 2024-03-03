5c4a125091bfbd73e099fb591d20277be7c7c7c4
Jason Milad <jasonmilad29@gmail.com>
2024-03-02 21:57:57 -0500

    Fixed bug

--- src/keys.rs
+++ src/keys.rs
@@ -2,7 +2,7 @@ use rand_core::{OsRng};
 use std::{thread, time};
 pub mod key {
 
-    fn generate_key() -> [0u8:32] {
+    fn generate_key() -> [u8:32] {
         println!("Please wiggle your mouse around for a few seconds to increase the entropy, and thus security of your private key!");
         println!("You have 5 seconds to wiggle your mouse around");
         thread::sleep(time::Duration::from_secs(5));
