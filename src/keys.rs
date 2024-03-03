27904a73d11970154e7f01c744e8187c542f91bb
Jason Milad <jasonmilad29@gmail.com>
2024-03-02 21:57:06 -0500

    Added random key generator

---
+++ src/keys.rs
@@ -0,0 +1,14 @@
+use rand_core::{OsRng};
+use std::{thread, time};
+pub mod key {
+
+    fn generate_key() -> [0u8:32] {
+        println!("Please wiggle your mouse around for a few seconds to increase the entropy, and thus security of your private key!");
+        println!("You have 5 seconds to wiggle your mouse around");
+        thread::sleep(time::Duration::from_secs(5));
+        let mut key = [0u8: 32]; // 256 bit random number
+        OsRng.fill_bytes(&mut key);
+        key
+    }
+
+}
