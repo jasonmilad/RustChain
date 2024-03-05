094d3daec7c7b2e5f6f4708db9c54351064bfcaf
Jason Milad <jasonmilad29@gmail.com>
2024-03-04 22:39:17 -0500

    Added private key generator unit test

--- src/keys.rs
+++ src/keys.rs
@@ -1,39 +1,62 @@
-use rand_core::OsRng;
-use std::{thread, time};
-use k256;
-use u256;
-pub mod key {
+pub mod keys {
+    use rand_core::{RngCore, OsRng};
+    use std::{thread, time};
+    use crate::u256::U256;
+    use rand_core::Error;
 
-    struct PrivateKey {
-        key: u256,
+    pub struct PrivateKey {
+        pub key: U256,
     }
 
 
     //hexadecimal and raw binary to be used for private key.
     impl PrivateKey {
-        pub fn new() -> PrivateKey {
+        pub fn new() -> Result<PrivateKey, Error> {
             // wiggling mouse increases entropy which increases randomness of private key
             println!("Please wiggle your mouse around for a few seconds to increase the entropy, and thus security of your private key!");
             println!("You have 5 seconds to wiggle your mouse around");
             thread::sleep(time::Duration::from_secs(5));
             let mut array = [0u8; 32]; // 256 bit zero-initialized number.
-            OsRng.fill_bytes(&mut array); // fill the number with random numbers.
-            let key = u256 {array}; //create private key, which is a 256 bit number.
+            let zero_array = [0u8;32];
+
+            //make sure that the private key is greater than 0
+            while array == zero_array {
+                match OsRng.try_fill_bytes(&mut array) { // safe fill array with random
+                    Err(e) => return Err(e),
+                    _ => ()
+                }
+            }
+            println!("{:?}", array);
+            let key = U256::new(array); //create private key value, which is a 256 bit number.
             println!("New private key(binary): {:?}", key.get_binary()); //print private key
             println!("New private key(hexadecimal): {:?}", key.get_hex()); //print private key
-            key // transfer ownership out.
+            Ok(PrivateKey{key}) // transfer ownership out.
         }
     }
 
     //compressed or uncompressed public key are the two formats. 
     //uncompressed starts with 04, compressed starts with 02 or 03
-    //02 for even, 03 for odd (two possiblities due to square root)
+    //02 for even, 03 for odd (two possiblities due to square root of y coordinate)
     //TODO: implement secp256k1 from scratch.
-    fn generate_public_key() -> String { //return public key
+    // fn generate_public_key() -> String { //return public key
 
-        //uncompressed form of G
-        let G = "79BE667E F9DCBBAC 55A06295 CE870B07 029BFCDB 2DCE28D9 59F2815B 16F81798 483ADA77".split(" "); //returns an iterator.
+    //     //uncompressed form of G
+    //     //TODO: move this to seperate module and file.
+    //     let G = "79BE667E F9DCBBAC 55A06295 CE870B07 029BFCDB 2DCE28D9 59F2815B 16F81798 483ADA77".split(" "); //returns an iterator.
+    // }
+}
 
-        let curve = k256::Secp256k1 {};
+#[cfg(test)]
+mod tests {
+    use crate::keys::keys;
+    #[test]
+    fn private_key_valid() {
+        let private_key = keys::PrivateKey::new().unwrap();
+        let key = private_key.key;
+        let zero_array = [0u128;2];
+        assert_ne!(key.arr128, zero_array);
+        assert_eq!(key.arr128.len(), 2);
+        assert_eq!(key.get_hex().chars().count(), 64);
+        assert_eq!(key.get_binary().chars().count(), 256);
     }
 }
